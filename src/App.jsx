7fa1e834f7436d78c3dd2b2ffe5063129e37d9f2
Kevin Dam <kevindam96@gmail.com>
2021-10-02 14:01:29 -0700

    Formalize app layout and text. Remove header

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Layout } from 'antd';
+import { GithubFilled } from '@ant-design/icons';
 import Maze from './components/Maze';
 
 import 'antd/dist/antd.css';
 import './index.scss';
 import styles from './App.module.scss';
 
-const { Header, Footer, Content } = Layout;
+const { Footer, Content } = Layout;
 
 const App = function MainApp() {
   return (
     <Layout className={styles.layout}>
-      <Header>Maze</Header>
       <Content className={styles.content}>
         <Maze />
       </Content>
-      <Footer>Footer</Footer>
+      <Footer className={styles.footer}>
+        <div className={styles['footer-items']}>
+          <p>Maze Generator & Solver © 2021 Created by Kevin Dam</p>
+          <a
+            className={styles['anchor-color']}
+            href="https://github.com/kevindam96/maze-generator"
+          >
+            <GithubFilled className={styles.icon} />
+          </a>
+        </div>
+      </Footer>
     </Layout>
   );
 };
